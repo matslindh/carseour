70cf2da0e4dc915f3b8170a7cc25746c97285f69
Mats Lindh <mats@lindh.no>
2015-05-25 14:57:05 +0200

    Add setup.py to allow package installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='carseour',
+      version='0.2',
+      description='Project Cars API implementation for Python 3',
+      author='Mats Lindh',
+      author_email='mats@lindh.no',
+      url='https://github.com/matslindh/carseour/',
+      keywords='games racing projectcars',
+     )
