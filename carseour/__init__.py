da825b76933d97c223aa28674ba1a145b2b9db66
Mats Lindh <mats@lindh.no>
2015-05-25 19:51:24 +0200

    Whitespace

--- carseour/__init__.py
+++ carseour/__init__.py
@@ -1,5 +1,6 @@
 import mmap
-from carseour.definitions import GameInstance, SHARED_MEMORY_VERSION
+from carseour.models import GameInstance
+from carseour.definitions import SHARED_MEMORY_VERSION
 
 def _get_mmapped():
     # could we use ctypes.sizeof(GameInstance) instead here? A too large value results in access denied,
